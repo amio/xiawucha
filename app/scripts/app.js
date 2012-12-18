565225e4916708301dbc233bcc1baad70941fccb
Amio <amio.cn@gmail.com>
2012-12-19 00:00:31 +0800

    Init with "yeoman init angular"

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var xiawuchaApp = angular.module('xiawuchaApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+xiawuchaApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
