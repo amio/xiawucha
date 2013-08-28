f37ace1c1f11ff171b5eacfdcc4b167ed80c1289
Amio <amio.cn@gmail.com>
2013-08-28 17:18:24 +0800

    + 自动记忆用户名

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,15 +2,24 @@
 
 App.controller('MainCtrl',
  function ($scope, angularFireCollection) {
+
   $scope.newItem = {
+    name: '',
     desc: ''
   };
+
+  // Sync "name" with localStorage.
+  if(window.localStorage){
+    $scope.newItem.name = localStorage.getItem('user-name') || '';
+    $scope.$watch('newItem.name',function(newValue){
+      localStorage.setItem('user-name',newValue);
+    });
+  }
 
   var fireRef = new Firebase('https://amio.firebaseio.com/list');
   $scope.list = angularFireCollection( fireRef.limit(50), $scope, "list", [] );
 
   $scope.addOne = function(newItem){
-    console.log(1);
     $scope.list.add({
       name: newItem.name,
       desc: newItem.desc
