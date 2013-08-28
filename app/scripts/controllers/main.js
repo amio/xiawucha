635e9dc0bb8dc58386d4ec44a8e2bb9fa79ff5e9
Amio <amio.cn@gmail.com>
2013-08-28 18:03:34 +0800

    更换数据源到xiawucha.firebaseio.com；按日记录订单

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,17 +9,19 @@ App.controller('MainCtrl',
   };
 
   // Sync "name" with localStorage.
-  if(window.localStorage){
-    $scope.newItem.name = localStorage.getItem('user-name') || '';
-    $scope.$watch('newItem.name',function(newValue){
-      localStorage.setItem('user-name',newValue);
+  if( window.localStorage ){
+    $scope.newItem.name = localStorage.getItem( 'user-name' ) || '';
+    $scope.$watch( 'newItem.name', function( newValue ){
+      localStorage.setItem( 'user-name', newValue );
     });
   }
 
-  var fireRef = new Firebase('https://amio.firebaseio.com/list');
-  $scope.list = angularFireCollection( fireRef.limit(50), $scope, "list", [] );
+  var todayNum = Math.floor( Date.now()/ (60*60*24*1000) + 0.333334 );
+  var fireRef = new Firebase( 'https://xiawucha.firebaseio.com/orderlists/' + todayNum );
+  console.log(fireRef);
+  $scope.list = angularFireCollection( fireRef.limit(50), $scope, "list", {} );
 
-  $scope.addOne = function(newItem){
+  $scope.addOne = function( newItem ){
     $scope.list.add({
       name: newItem.name,
       desc: newItem.desc
